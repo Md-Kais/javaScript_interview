304650fbe7623f42c4c64f08084039664838119e
Md-Kais <mdkais3@gmail.com>
2021-02-04 14:41:10 +0600

    hoisting var let const

---
+++ scope.js
@@ -0,0 +1,11 @@
+//https://web.programming-hero.com/course/video/web-3-23-7-scope-block-scope-access-outer-scope-variable
+//var is motgherfucjer
+//hoisting//more at:https://www.thatjsdude.com/jsConcepts/concepts/scope.html
+//var can accessible for the parent scope.it's called hoyesting.upper level can acess the value
+//let,const isnot allow hoyesting.
+//so var is mofo[don't use var]var is real madrid er slave
+//var sees slav memes
+//that Js dude blows our mind
+//that Js Mate
+
+//https://www.thatjsdude.com/jsConcepts/concepts/scope.html
